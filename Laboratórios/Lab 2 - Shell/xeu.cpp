3cb82411aa03f1f4517ab743904b6e1205fc382c
matheusps <cout.matheusps@gmail.com>
2019-09-05 23:17:33 -0300

    Added bg and xjobs :: missing threads

--- Laboratórios/Lab 2 - Shell/xeu.cpp
+++ Laboratórios/Lab 2 - Shell/xeu.cpp
@@ -8,152 +8,85 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <thread>
+#include <utility>
 
 using namespace xeu_utils;
 using namespace std;
 
-// This function is just to help you learn the useful methods from Command
-void io_explanation(const Command &command)
-{
-  // Let's use this input as example: (ps aux >out >o2 <in 2>er >ou)
-  // we would print "$       io(): [0] >out [1] >o2 [2] <in [3] 2>er [4] >ou"
-  cout << "$       io():";
-  for (size_t i = 0; i < command.io().size(); i++)
-  {
-    IOFile io = command.io()[i];
-    cout << " [" << i << "] " << io.repr();
-    // Other methods - if we had 2>file, then:
-    // io.fd() == 2
-    // io.is_input() == false ('>' is output)
-    // io.is_output() == true
-    // io.path() == "file"
-  }
-}
+vector< pair<pid_t,Command> > jobs;
 
-// This function is just to help you learn the useful methods from Command
-void command_explanation(const Command &command)
-{
-
-  /* Methods that return strings (useful for debugging & maybe other stuff) */
-
-  // This prints the command in a format that can be run by our xeu. If you
-  // run the printed command, you will get the exact same Command
-  {
-    // Note: command.repr(false) doesn't show io redirection (i.e. <in >out)
-    cout << "$     repr(): " << command.repr() << endl;
-    cout << "$    repr(0): " << command.repr(false) << endl;
-    // cout << "$   (string): " << string(command) << endl; // does the same
-    // cout << "$ operator<<: " << command << endl; // does the same
-  }
-
-  // This is just args()[0] (e.g. in (ps aux), name() is "ps")
-  {
-    cout << "$     name(): " << command.name() << endl;
-  }
-
-  // Notice that args[0] is the command/filename
-  {
-    cout << "$     args():";
-    for (int i = 0; i < command.args().size(); i++)
-    {
-      cout << " [" << i << "] " << command.args()[i];
+void wait_process(pid_t pid) {
+  waitpid(pid, NULL, 0);
+  int i = 0;
+  while(i < jobs.size()){
+    if(jobs[i].first == pid) {
+      jobs.erase(jobs.begin() + i);
+      break;
     }
-    cout << endl;
-  }
-
-  /* Methods that return C-string (useful in exec* syscalls) */
-
-  // this is just the argv()[0] (same as name(), but in C-string)
-  {
-    printf("$ filename(): %s\n", command.filename());
-  }
-
-  // This is similar to args, but in the format required by exec* syscalls
-  // After the last arg, there is always a NULL pointer (as required by exec*)
-  {
-    printf("$     argv():");
-    for (int i = 0; command.argv()[i]; i++)
-    {
-      printf(" [%d] %s", i, command.argv()[i]);
-    }
-    printf("\n");
-  }
-
-  io_explanation(command);
-}
-
-// This function is just to help you learn the useful methods from Command
-void commands_explanation(const vector<Command> &commands)
-{
-  // Shows a representation (repr) of the command you input
-  // cout << "$ Command::repr(0): " << Command::repr(commands, false) << endl;
-  cout << "$ Command::repr(): " << Command::repr(commands) << endl
-       << endl;
-
-  // Shows details of each command
-  for (int i = 0; i < commands.size(); i++)
-  {
-    cout << "# Command " << i << endl;
-    command_explanation(commands[i]);
-    cout << endl;
+    i++;
   }
 }
 
-void exec(Command &command) {
-  execvp(command.filename(), command.argv());
-}
-
-Command chopBg(Command &command) {
-  Command chopedCommand;
-  for (int i = 1; i < command.args().size(); i++) {
-    chopedCommand.add_arg(command.argv()[i]);
-  }
-  return chopedCommand;
-}
-
-int process(const vector<Command> &commands)
-{
-
-  Command command = commands[0];
+pid_t exec(Command cm) {
   pid_t pid = fork();
-  thread th1;
-
-  if (pid == 0)
-  {
-    if (!strcmp(command.filename(), "bg"))
-    {
-      Command newCommand = chopBg(command);
-      exec(newCommand);
+  if(pid == 0) {
+    execvp(cm.filename(), cm.argv());
+    string err;
+    switch (errno) {
+      case ENOENT:
+        err = "Command not found -> ";
+        break;
+      
+      default:
+        err = "Could not execute -> ";
+        break;
     }
-
-    exec(command);
+    cerr << "ERROR: " << err << cm.filename() << endl;
+    exit(1);
   }
-
-  wait(NULL);
-  return 0;
+  return pid;
 }
 
-bool exit(const vector<Command> &commands)
-{
-  for (int i = 0; i < commands.size(); i++)
-  {
-    if (!strcmp(commands[i].filename(), "exit"))
-    {
-      return false;
-    }
-  }
+bool exit(const vector<Command> cms) {
+  for (int i = 0; i < cms.size(); i++)
+    if (!strcmp(cms[i].filename(), "exit")) return false;
+  
   return true;
 }
 
-int main()
-{
+void xjobs() {
+  for (auto job : jobs)
+    cout << "pid: " << job.first << " | command: " << " " << job.second.repr() << endl;
+}
 
-  vector<Command> commands = StreamParser().parse().commands();
+int main() {
 
-  while (exit(commands))
-  {
-    process(commands);
-    commands = StreamParser().parse().commands();
+  auto cms = StreamParser().parse().commands();
+
+  while (exit(cms)) {
+    Command cm = cms[0];
+
+    if (!strcmp(cm.filename(), "xjobs")) {
+      xjobs();
+    } else if (!strcmp(cm.filename(), "bg")) {
+      auto bgCm = Command();
+      auto args = vector<string>(cm.args());
+      
+      args.erase(args.begin());
+
+      for (auto arg : args) bgCm.add_arg(arg);
+
+      pid_t pid = exec(bgCm);
+      jobs.push_back(make_pair(pid, bgCm));
+      //thread t(wait_process, pid);
+      //t.detach();
+    } else {
+      pid_t pid = exec(cm);
+      waitpid(pid, NULL, 0);
+    }
+
+    wait(NULL);
+    cms = StreamParser().parse().commands();
   }
 
   return 0;
